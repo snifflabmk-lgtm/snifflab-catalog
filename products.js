fbfb924b24475258d38c62511bc4e7737d08f510
snifflabmk-lgtm <sniff.lab.mk@gmail.com>
2026-07-23 20:27:34 +0200

    Create products.js

---
+++ products.js
@@ -0,0 +1,151 @@
+window.products = [
+  {
+    id: "afnan-9pm",
+    name: "Afnan 9PM",
+    brand: "Afnan",
+    image: "afnan-9pm.png",
+    category: "Арапски",
+    categorySlug: "arapski",
+    gender: "Машки",
+    prices: {
+      3: 250,
+      5: 350,
+      10: 500,
+      20: 900
+    },
+    seasons: ["Есен", "Зима"],
+    notes: [
+      "Ванила",
+      "Јаболко",
+      "Цимет",
+      "Тонка",
+      "Лаванда",
+      "Амбер"
+    ],
+    occasions: [
+      "Секојдневно носење",
+      "Вечерни излегувања"
+    ]
+  },
+
+  {
+    id: "afnan-9pm-nightout",
+    name: "Afnan 9PM Night Out",
+    brand: "Afnan",
+    image: "afnan-9pm-nightout.png",
+    category: "Арапски",
+    categorySlug: "arapski",
+    gender: "Машки",
+    prices: {
+      3: 350,
+      5: 500,
+      10: 750,
+      20: 1350
+    },
+    seasons: ["Пролет", "Есен", "Зима"],
+    notes: [
+      "Питахаја",
+      "Тонка грав",
+      "Карамел",
+      "Лаванда",
+      "Коњак",
+      "Велур"
+    ],
+    occasions: [
+      "Вечерни излегувања"
+    ]
+  },
+
+  {
+    id: "afnan-9pm-rebel",
+    name: "Afnan 9PM Rebel",
+    brand: "Afnan",
+    image: "afnan-9pm-rebel.png",
+    category: "Арапски",
+    categorySlug: "arapski",
+    gender: "Машки",
+    prices: {
+      3: 300,
+      5: 450,
+      10: 600,
+      20: 1100
+    },
+    seasons: ["Пролет", "Лето", "Есен", "Зима"],
+    notes: [
+      "Ананас",
+      "Суво дрво",
+      "Дабов мов",
+      "Сива амбра",
+      "Карамел",
+      "Кедар"
+    ],
+    occasions: [
+      "Вечерни излегувања",
+      "Романтичен состанок",
+      "Забава",
+      "Клуб"
+    ]
+  },
+
+  {
+    id: "afnan-supremacy-collectors-edition",
+    name: "Afnan Supremacy Collector's Edition",
+    brand: "Afnan",
+    image: "afnan-supremacy-collectors-edition.webp",
+    category: "Арапски",
+    categorySlug: "arapski",
+    gender: "Машки",
+    prices: {
+      3: 300,
+      5: 500,
+      10: 750,
+      20: 1350
+    },
+    seasons: ["Пролет", "Лето", "Есен", "Зима"],
+    notes: [
+      "Ананас",
+      "Бергамот",
+      "Бели цветови",
+      "Цвет од портокал",
+      "Дабов мов",
+      "Мошус"
+    ],
+    occasions: [
+      "Секојдневно носење",
+      "Романтичен состанок",
+      "Деловни средби",
+      "Специјални пригоди"
+    ]
+  },
+
+  {
+    id: "afnan-turathi-blue",
+    name: "Afnan Turathi Blue",
+    brand: "Afnan",
+    image: "afnan-turathi-blue.png",
+    category: "Арапски",
+    categorySlug: "arapski",
+    gender: "Машки",
+    prices: {
+      3: 250,
+      5: 350,
+      10: 550,
+      20: 950
+    },
+    seasons: ["Пролет", "Лето"],
+    notes: [
+      "Цитруси",
+      "Дрвенести ноти",
+      "Килибар",
+      "Мошус",
+      "Зачини",
+      "Пачули"
+    ],
+    occasions: [
+      "Секојдневно носење",
+      "Работа",
+      "Романтичен состанок",
+      "Вечерни излегувања"
+    ]
+  }
+];
