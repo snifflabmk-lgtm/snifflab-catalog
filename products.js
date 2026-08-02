e5774c1d37397cf7c3cb0ef78aafb20d9cd7522f
snifflabmk-lgtm <sniff.lab.mk@gmail.com>
2026-08-02 16:54:41 +0200

    Mark Supremacy Collector's Edition out of stock

--- products.js
+++ products.js
@@ -93,7 +93,7 @@ window.products = [
     categorySlug: "arapski",
     gender: "Машки",
     badges: ["bestseller"],
-    prices: { 3: 300, 5: 500, 10: 750, 20: null },
+    prices: { 3: null, 5: null, 10: null, 20: null },
     seasons: ["Пролет", "Лето", "Есен", "Зима"],
     notes: [
       "Ананас",
