7d05b5de53b8c686c42ac5b8d749fda9515312cf
snifflabmk-lgtm <sniff.lab.mk@gmail.com>
2026-08-09 15:05:05 +0200

    Mark Afnan 9PM Night Out as sold out

--- products.js
+++ products.js
@@ -36,8 +36,8 @@ window.products = [
     category: "Арапски",
     categorySlug: "arapski",
     gender: "Машки",
-    badges: ["bestseller"],
-    prices: { 3: 350, 5: 500, 10: 750, 20: null },
+    badges: ["soldout", "bestseller"],
+    prices: { 3: null, 5: null, 10: null, 20: null },
     seasons: ["Пролет", "Есен", "Зима"],
     notes: [
       "Питахаја",
