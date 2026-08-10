36ea7246349872edf4a530c30f690844b8b490fa
snifflabmk-lgtm <sniff.lab.mk@gmail.com>
2026-08-11 00:58:18 +0200

    Update products.js

--- products.js
+++ products.js
@@ -1190,11 +1190,11 @@ window.products = [
     category: "Арапски",
     categorySlug: "arapski",
     gender: "Машки",
-    badges: ["bestseller"],
+  badges: ["top", "soldout"],
     prices: {
-      3: 300,
-      5: 500,
-      10: 750,
+      3: null,
+      5: null,
+      10: null,
       20: null
     },
     seasons: ["Пролет", "Лето"],
